139c77bda4517a1fe8ed10b6389c46e46a93a4e2
rgb-hmikedev <hmontemayorrios2@gmail.com>
2024-08-07 13:19:00 -0600

    agregando menus y JS a la landing

---
+++ JS/lightbox.js
@@ -0,0 +1,20 @@
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenesLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('imagen-light');
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+        
+       
+    })
+})
+
+
+
+const aparecerImagen = (imagen)=> {
+   imagenesLight.src = imagen;
+   contenedorLight.classList.toogle('show');
+
+};
+
